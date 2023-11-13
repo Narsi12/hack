e2d65cd23827180737434322334642eda54c7c7e
Kalyani <kalyani24@gmail.com>
2023-11-13 14:44:23 +0530

    admin changess

--- app/admin.py
+++ app/admin.py
@@ -3,15 +3,23 @@ from .models import USER_details,USER_Entry,Driver_Entry,Hospital
 
 
 class DriverEntryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'hospital_name', 'vehicle_num', 'phone_num', 'license'
+    list_display = ['name', 'hospital_name', 'vehicle_num', 'phone_num', 'location','email'
     ]
 
 admin.site.register(Driver_Entry, DriverEntryAdmin)
 
+class HospitalEntryAdmin(admin.ModelAdmin):
+    list_display = ['hospital_name','mobile', 'email', 'location'
+    ]
+
+class UserEntryAdmin(admin.ModelAdmin):
+    list_display = ['name','email','phone_number'
+    ]
+
 admin.site.register(USER_details)
-admin.site.register(USER_Entry)
+admin.site.register(USER_Entry,UserEntryAdmin)
 # admin.site.register(Driver_Entry)
-admin.site.register(Hospital)
+admin.site.register(Hospital,HospitalEntryAdmin)
 
 
 # class AgentAdmin(admin.ModelAdmin):

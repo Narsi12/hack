f26a60b1126e79812671d4131f584b2811747e12
Kalyani <kalyani24@gmail.com>
2023-11-15 09:15:11 +0530

    change

--- myproject/settings.py
+++ myproject/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = DATABASES = {
     'default': {
         'ENGINE': 'djongo',   
-        'NAME': 'ambulance_data',
+        'NAME': 'ambulance_tracker',
     }
 }
 
